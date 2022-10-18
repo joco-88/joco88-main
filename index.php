00fe97169913546d4422c560eb7fb62255069242
joco-88 <jocodigitalweb@gmail.com>
2022-10-18 16:20:19 -0500

    Minor change

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
 
       </header>
       <main>
-        <p>Test Paragraph and testing css. Now testing deploy AGAIN.</p>
+        <p>Test Paragraph and testing css. Now testing deploy again.</p>
         </main>
   </body>
 </html>
