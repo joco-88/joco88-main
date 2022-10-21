18f9174dff87535e90776718d7856199d2706f23
joco-88 <jocodigitalweb@gmail.com>
2022-10-20 21:37:29 -0500

    Testing from MacBook vscode

--- index.php
+++ index.php
@@ -12,6 +12,7 @@
       <main>
         <p>Test Paragraph and testing css. Now testing deploy again.</p>
           <p>Testing to see if this auto published to git after saving. Go!</p>
+          <p>Now testing that VS code on my Macbook works</p>
         </main>
   </body>
 </html>
