c89a86502130d03ddb4926c03b06a994ec99fd4c
joco-88 <jocodigitalweb@gmail.com>
2022-10-21 03:28:06 +0000

    Oct 20, 2022, 10:28 PM

--- index.php
+++ index.php
@@ -12,7 +12,8 @@
       <main>
         <p>Test Paragraph and testing css. Now testing deploy again.</p>
           <p>Testing to see if this auto published to git after saving. Go!</p>
-          <p>Now testing that VS code on my Macbook works.... again!</p>
+          <p>Now testing that VS code on my Macbook works.... again!<br>
+          Testing the auto save feature.</p>
         </main>
   </body>
 </html>
