aab1da9fe2cb85b5d09bc6eca69dc021272239df
joco-88 <jocodigitalweb@gmail.com>
2022-10-24 16:40:34 -0500

    Updated header2.php.

--- templates/header2.php
+++ templates/header2.php
@@ -10,10 +10,42 @@
     </title>
   </head>
     <header>
-      <div class="bg-primary text-white p-5 text-center">
-        <h1 class=>Joco88.com</h1>
-<nav>
-    <p><center>Nav Links Go Here</center></p>
-</nav>
+      <div class="container">
+        <nav class="navbar navbar-expand-lg main-nav px-0">
+          <a class="navbar-brand" href="/mojo">
+  					      <img src="http://rajeshdas.com/assets/images/logo.svg" alt="rajeshdas.com">
+  					    </a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#mainMenu" aria-controls="mainMenu" aria-expanded="false" aria-label="Toggle navigation">
+                          <span class="icon-bar icon-bar-1"></span>
+                          <span class="icon-bar icon-bar-2"></span>
+                          <span class="icon-bar icon-bar-3"></span>
+                      </button>
+          <div class="collapse navbar-collapse" id="mainMenu">
+            <ul class="navbar-nav ml-auto text-uppercase f1">
+              <li>
+                <a href="#home" class="active active-first">home</a>
+              </li>
+              <li>
+                <a href="#about">about us</a>
+              </li>
+              <li>
+                <a href="#service">services</a>
+              </li>
+              <li>
+                <a href="#project">projects</a>
+              </li>
+              <li>
+                <a href="#team">team</a>
+              </li>
+              <li>
+                <a href="#testimony">testimonils</a>
+              </li>
+              <li>
+                <a href="#contact">contact</a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </div>
     </header>
   <body>
