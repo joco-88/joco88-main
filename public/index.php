27dcdb4c00bb0f8fa8e2440051846775793246b2
joco-88 <jocodigitalweb@gmail.com>
2022-10-24 12:19:50 -0500

    Updated index.php.

--- public/index.php
+++ public/index.php
@@ -6,7 +6,7 @@ $PageTitle="Home";
 include('header.php');
 ?>
 
-//Body starts here as body tag opens at end of Header.php file
+<!-- Body starts here as body tag opens at end of Header.php file -->
       <main>
         <p>Test Paragraph now that I have root access to public folder working.</p>
 
