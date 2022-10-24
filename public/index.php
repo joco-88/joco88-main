17cd54cde5f01033f3bccc5f4138f0379d0251fb
joco-88 <jocodigitalweb@gmail.com>
2022-10-24 17:12:36 -0500

    Updated index.php.

--- public/index.php
+++ public/index.php
@@ -4,16 +4,15 @@ include('/home/joco88.com/public_html/templates/header.php');
 ?>
 
 <!-- Body starts here as body tag opens at end of Header.php file -->
-      <main>
-        <div class="container-fluid">
-        <p>Test Paragraph now that I have root access to public folder working. Test Keek</p>
-      </div>
-      </p>
-  </main>
-    <footer>
-        <?php
-        include('/home/joco88.com/public_html/templates/footer.php');
-        ?>
-    </footer>
-  </body>
+<main>
+  <div class="container-fluid">
+      <p>Test Paragraph now that I have root access to public folder working. Test Keek</p>
+  </div>
+</main>
+  <footer>
+    <?php
+      include('/home/joco88.com/public_html/templates/footer.php');
+    ?>
+  </footer>
+</body>
 </html>
