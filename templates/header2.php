5d2fd6220f367f2ce7090484c892f7d410104fb5
joco-88 <jocodigitalweb@gmail.com>
2022-10-24 16:50:37 -0500

    Updated header2.php.

--- templates/header2.php
+++ templates/header2.php
@@ -4,49 +4,16 @@
     <meta http-equiv="content-type" content="text/html; charset=UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
-    <link href="/css/styles2.scss" rel="stylesheet">
+    <link href="/css/styles.css" rel="stylesheet">
     <title>
       <?php echo isset($PageTitle) ? $PageTitle : "Joco88.com"?>
     </title>
   </head>
-    <header class="main-header">
-      <div class="container">
-        <nav class="navbar navbar-expand-lg main-nav px-0">
-          <a class="navbar-brand" href="/mojo">
-  					      <img src="https://IMAGE" alt="jocodigital">
-  					    </a>
-          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#mainMenu" aria-controls="mainMenu" aria-expanded="false" aria-label="Toggle navigation">
-                          <span class="icon-bar icon-bar-1"></span>
-                          <span class="icon-bar icon-bar-2"></span>
-                          <span class="icon-bar icon-bar-3"></span>
-                      </button>
-          <div class="collapse navbar-collapse" id="mainMenu">
-            <ul class="navbar-nav ml-auto text-uppercase f1">
-              <li>
-                <a href="#home" class="active active-first">home</a>
-              </li>
-              <li>
-                <a href="#about">about us</a>
-              </li>
-              <li>
-                <a href="#service">services</a>
-              </li>
-              <li>
-                <a href="#project">projects</a>
-              </li>
-              <li>
-                <a href="#team">team</a>
-              </li>
-              <li>
-                <a href="#testimony">testimonils</a>
-              </li>
-              <li>
-                <a href="#contact">contact</a>
-              </li>
-            </ul>
-          </div>
-        </nav>
-      </div>
-      <!-- /.container -->
+    <header>
+      <div class="bg-primary text-white p-5 text-center">
+        <h1 class=>Joco88.com</h1>
+<nav>
+    <p><center>Nav Links Go Here</center></p>
+</nav>
     </header>
   <body>
