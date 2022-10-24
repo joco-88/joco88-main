66cf28a84ae6cd490ae0ba42591b77d606461b02
joco-88 <jocodigitalweb@gmail.com>
2022-10-24 16:38:08 -0500

    Updated header2.php.

--- templates/header2.php
+++ templates/header2.php
@@ -10,49 +10,10 @@
     </title>
   </head>
     <header>
-      <div class="site-mobile-menu site-navbar-target">
-          <div class="site-mobile-menu-header">
-            <div class="site-mobile-menu-close mt-3">
-              <span class="icon-close2 js-menu-toggle"></span>
-            </div>
-          </div>
-          <div class="site-mobile-menu-body"></div>
-        </div>
-
-
-
-        <header class="site-navbar site-navbar-target" role="banner">
-
-        <div class="container">
-          <div class="row align-items-center position-relative">
-
-            <div class="col-3">
-              <div class="site-logo">
-                <a href="index.php" class="font-weight-bold">Brand</a>
-              </div>
-            </div>
-
-            <div class="col-9  text-right">
-
-
-              <span class="d-inline-block d-lg-none"><a href="#" class="text-primary site-menu-toggle js-menu-toggle py-5"><span class="icon-menu h3 text-white"></span></a></span>
-
-
-              <nav class="site-navigation text-right ml-auto d-none d-lg-block" role="navigation">
-                <ul class="site-menu main-menu js-clone-nav ml-auto ">
-                  <li class="active"><a href="#" class="nav-link">Home</a></li>
-                  <li><a href="#" class="nav-link">Portfolio</a></li>
-                  <li><a href="#" class="nav-link">About</a></li>
-                  <li><a href="#" class="nav-link">Services</a></li>
-                  <li><a href="#" class="nav-link">Blog</a></li>
-                  <li><a href="#" class="nav-link">Contact</a></li>
-                </ul>
-              </nav>
-            </div>
-
-
-          </div>
-        </div>
+      <div class="bg-primary text-white p-5 text-center">
+        <h1 class=>Joco88.com</h1>
+<nav>
+    <p><center>Nav Links Go Here</center></p>
 </nav>
     </header>
   <body>
