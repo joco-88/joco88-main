6343020eab6c696ed2c40656fc13741df543cbd6
joco-88 <jocodigitalweb@gmail.com>
2022-10-24 14:10:06 -0500

    Updated header.php.

--- templates/header.php
+++ templates/header.php
@@ -3,8 +3,9 @@
   <head>
     <meta http-equiv="content-type" content="text/html; charset=UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"">
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet">
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js"></script>
+    <link href="css/styles.css" rel="sylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js"></script>
     <title>
       <?php echo isset($PageTitle) ? $PageTitle : "Joco88.com"?>
     </title>
