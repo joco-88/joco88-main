43caf0a9b97e85e16048d1b7e521bf614c5c1391
joco-88 <jocodigitalweb@gmail.com>
2022-10-24 12:05:08 -0500

    Updated header.php.

--- templates/header.php
+++ templates/header.php
@@ -4,7 +4,7 @@
     <meta http-equiv="content-type" content="text/html; charset=UTF-8">
     <title><?= isset($PageTitle) ? $PageTitle : "Joco88.com"?></title>
     <!-- Additional tags here -->
-    <?php if (function_exists('customPageHeader')){
+    <?php if (function_exists('joePageHeader')){
       customPageHeader();
     }?>
   </head>
