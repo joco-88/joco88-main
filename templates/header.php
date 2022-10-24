f88b3cb3d224f769373973c6d6b883559a861273
joco-88 <jocodigitalweb@gmail.com>
2022-10-24 14:13:17 -0500

    Updated header.php.

--- templates/header.php
+++ templates/header.php
@@ -3,7 +3,7 @@
   <head>
     <meta http-equiv="content-type" content="text/html; charset=UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"">
-    <link href="css/styles.css" rel="sylesheet">
+    <link href="/css/styles.css" rel="sylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js"></script>
     <title>
