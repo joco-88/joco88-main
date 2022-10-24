1f2ae9f34875b9588f16269d4ab7c73abf2c1165
joco-88 <jocodigitalweb@gmail.com>
2022-10-24 12:05:02 -0500

    Updated index.php.

--- public/index.php
+++ public/index.php
@@ -1,5 +1,16 @@
 <?php
 $PageTitle="Home";
+
+<header>
+function joePageHeader(){?>
+<?php }
+
+include_once('header.php');
+
+//body contents go here
+
+include_once('footer.php');
+?>
       </header>
       <main>
         <p>Test Paragraph now that I have root access to public folder working.</p>
