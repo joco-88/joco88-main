29c5cb35b52ec02dbc942c98496012910f4bff9b
joco-88 <jocodigitalweb@gmail.com>
2022-10-24 16:33:54 -0500

    Updated header2.php.

---
+++ templates/header2.php
@@ -0,0 +1,45 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <link href="/css/styles.css" rel="stylesheet">
+    <title>
+      <?php echo isset($PageTitle) ? $PageTitle : "Joco88.com"?>
+    </title>
+  </head>
+    <header>
+      <     <div class="container">
+          <div class="row align-items-center position-relative">
+
+            <div class="col-3">
+              <div class="site-logo">
+                <a href="index.html" class="font-weight-bold">Brand</a>
+              </div>
+            </div>
+
+            <div class="col-9  text-right">
+
+
+              <span class="d-inline-block d-lg-none"><a href="#" class="text-primary site-menu-toggle js-menu-toggle py-5"><span class="icon-menu h3 text-white"></span></a></span>
+
+
+              <nav class="site-navigation text-right ml-auto d-none d-lg-block" role="navigation">
+                <ul class="site-menu main-menu js-clone-nav ml-auto ">
+                  <li class="active"><a href="#" class="nav-link">Home</a></li>
+                  <li><a href="#" class="nav-link">Portfolio</a></li>
+                  <li><a href="#" class="nav-link">About</a></li>
+                  <li><a href="#" class="nav-link">Services</a></li>
+                  <li><a href="#" class="nav-link">Blog</a></li>
+                  <li><a href="#" class="nav-link">Contact</a></li>
+                </ul>
+              </nav>
+            </div>
+
+
+          </div>
+        </div>
+</nav>
+    </header>
+  <body>
