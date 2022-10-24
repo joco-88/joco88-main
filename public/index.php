9232e2d231ddb4dd3eb18f7a4091394a46d80a7a
joco-88 <jocodigitalweb@gmail.com>
2022-10-24 14:21:34 -0500

    Updated index.php.

--- public/index.php
+++ public/index.php
@@ -7,6 +7,7 @@ include('/home/joco88.com/public_html/templates/header.php');
 
 <!-- Body starts here as body tag opens at end of Header.php file -->
       <main>
+        <div class="container-fluid">
         <p>Test Paragraph now that I have root access to public folder working.</p>
 
       </p>
