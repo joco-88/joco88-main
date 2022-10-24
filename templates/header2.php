aa8799f56b49865ccaf5e92ad0c8df98584db172
joco-88 <jocodigitalweb@gmail.com>
2022-10-24 16:35:43 -0500

    Updated header2.php.

--- templates/header2.php
+++ templates/header2.php
@@ -10,7 +10,20 @@
     </title>
   </head>
     <header>
-          <div class="container">
+      <div class="site-mobile-menu site-navbar-target">
+          <div class="site-mobile-menu-header">
+            <div class="site-mobile-menu-close mt-3">
+              <span class="icon-close2 js-menu-toggle"></span>
+            </div>
+          </div>
+          <div class="site-mobile-menu-body"></div>
+        </div>
+
+
+
+        <header class="site-navbar site-navbar-target" role="banner">
+
+        <div class="container">
           <div class="row align-items-center position-relative">
 
             <div class="col-3">
