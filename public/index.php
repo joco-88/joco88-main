fa9996fff9619d650b3f687a6bb11455a219728b
joco-88 <jocodigitalweb@gmail.com>
2022-10-25 13:13:53 -0500

    Updated index.php.

--- public/index.php
+++ public/index.php
@@ -9,7 +9,6 @@ include('/home/joco88.com/public_html/templates/header.php');
       <br><h1>Welcome!</h1>
       <p>Test Paragraph now that I have root access to public folder working.</p>
       <p>I am also testing out a navbar and bootstrap in general.</p>
-      <img src ="media/JustineDustinJoe.JPG" alt="Joe, Dustin,  Justine" width="445" height="492">;
   </div>
 </main>
   <footer>
