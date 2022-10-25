788760eb370df18666380ce9a8d5f39928a1b353
joco-88 <jocodigitalweb@gmail.com>
2022-10-25 18:33:13 -0500

    Updated contactme.php.

---
+++ public/contactme.php
@@ -0,0 +1,19 @@
+<?php
+$PageTitle="Home";
+include('/home/joco88.com/public_html/templates/header.php');
+?>
+
+<!-- Body starts here as body tag opens at end of Header.php file -->
+<main>
+  <div class="container-fluid">
+      <br><h1>Welcome!</h1>
+      <p>On this page, I will test a basic contact form with validations.</p>
+  </div>
+</main>
+  <footer>
+    <?php
+      include('/home/joco88.com/public_html/templates/footer.php');
+    ?>
+  </footer>
+</body>
+</html>
