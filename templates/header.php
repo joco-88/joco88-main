58d05df922f1832099aa94e82e16f2bdb6202167
joco-88 <jocodigitalweb@gmail.com>
2022-10-25 18:27:35 -0500

    Updated header.php.

--- templates/header.php
+++ templates/header.php
@@ -3,6 +3,7 @@
 <head>
     <meta http-equiv="content-type" content="text/html; charset=UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="icon" type="image/x-icon" href="/media/favicon.ico">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
     <link href="/css/stylesheet.css" rel="stylesheet">
     <link href="/css/font-awesome/font-awesome.min.css" rel="stylesheet">
