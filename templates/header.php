500dd4d31e7c0d8353ae3e0bf20afecdf9ea7fed
joco-88 <jocodigitalweb@gmail.com>
2022-10-25 12:07:09 -0500

    Updated header.php.

--- templates/header.php
+++ templates/header.php
@@ -4,6 +4,7 @@
     <meta http-equiv="content-type" content="text/html; charset=UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
     <link href="/css/styles.css" rel="stylesheet">
     <script src="https://use.fontawesome.com/63008d17a6.js"></script>
     <?php if (isset($extraStylesheet)) {
