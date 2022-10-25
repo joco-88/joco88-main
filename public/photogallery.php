c5ef50faf5c0f9a660a45a92a505c779d424eb69
joco-88 <jocodigitalweb@gmail.com>
2022-10-25 10:28:25 -0500

    Updated photogallery.php.

--- public/photogallery.php
+++ public/photogallery.php
@@ -36,6 +36,32 @@ include('/home/joco88.com/public_html/templates/header.php');
        <a data-gallery="manual" href="media/JustineDustinJoe.JPG">
   <img src="media/JustineDustinJoe.JPG">
 </a>
+
+<script>
+$('[data-gallery=manual]').click(function (e) {
+
+  e.preventDefault();
+
+  var items = [],
+    // get index of element clicked
+    options = {
+      index: $(this).index()
+    };
+
+  // looping to create images array
+  $('[data-gallery=manual]').each(function () {
+    let src = $(this).attr('href');
+    items.push({
+      src: src
+    });
+  });
+
+  new PhotoViewer(items, options);
+
+});
+</script>
+
+
   </div>
 </main>
   <footer>
