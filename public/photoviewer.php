01f2caf64bb092611b9432e55db3a20020f4edf3
joco-88 <jocodigitalweb@gmail.com>
2022-10-25 14:03:10 -0500

    Updated photoviewer.php.

--- public/photoviewer.php
+++ public/photoviewer.php
@@ -51,7 +51,7 @@ include('/home/joco88.com/public_html/templates/header.php');
       include('/home/joco88.com/public_html/templates/footer.php');
     ?>
 
-    <script src="https://code.jquery.com/jquery-3.6.1.min.js" integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ=" crossorigin="anonymous"></script>
+    <!-- <script src="https://code.jquery.com/jquery-3.6.1.min.js" integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ=" crossorigin="anonymous"></script> -->
     <script src="/js/ezoom.js"></script>
 
   </footer>
