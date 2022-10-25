662fd15039dbf76f13a3e88fb16cf839a4b3b38a
joco-88 <jocodigitalweb@gmail.com>
2022-10-25 18:35:17 -0500

    Updated footer.php.

--- templates/footer.php
+++ templates/footer.php
@@ -1,5 +1,5 @@
 <hr>
-<center><p>Footer will go here and include copyright and quick links.</p></center>
+<center><p>&copy; Joseph Condon & Joco88.com</p></center>
 <!-- JavaScript Bundle with Popper -->
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.min.js" integrity="sha384-IDwe1+LCz02ROU9k972gdyvl+AESN10+x7tBKgc9I5HFtuNz0wWnPclzo6p9vxnk" crossorigin="anonymous"></script>
