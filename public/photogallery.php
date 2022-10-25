c6e254e35c33708f44dd3f92ffa19c01dd94a7c7
joco-88 <jocodigitalweb@gmail.com>
2022-10-25 09:48:25 -0500

    Updated photogallery.php.

--- public/photogallery.php
+++ public/photogallery.php
@@ -1,8 +1,8 @@
 <?php
-$PageTitle="Photo Gallery";
+$PageTitle = "Photo Gallery";
+$extraStylesheet = "<link href="/css/photoviewer.css" rel="stylesheet">";
 include('/home/joco88.com/public_html/templates/header.php');
 ?>
-
 <!-- Body starts here as body tag opens at end of Header.php file -->
 <main>
   <div class="container-fluid">
@@ -15,5 +15,6 @@ include('/home/joco88.com/public_html/templates/header.php');
       include('/home/joco88.com/public_html/templates/footer.php');
     ?>
   </footer>
+  // Installing test scripts for photo viewer, only on this page.
 </body>
 </html>
