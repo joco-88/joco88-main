40861bca895a20cc9b11d2a304474d5e597c6b01
joco-88 <jocodigitalweb@gmail.com>
2022-10-25 10:00:08 -0500

    Updated photogallery.php.

--- public/photogallery.php
+++ public/photogallery.php
@@ -6,7 +6,7 @@ include('/home/joco88.com/public_html/templates/header.php');
 <!-- Body starts here as body tag opens at end of Header.php file -->
 <main>
   <div class="container-fluid">
-      <br><h1>Welcome!</h1>
+      <br><h1>Joe's Photo Viewer</h1>
       <p>This is my first test photo gallery.</p>
   </div>
   <div class="container-fluid">
