311829777187e7b327141cc62b50ec9ae63bcdd6
joco-88 <jocodigitalweb@gmail.com>
2022-10-25 19:13:41 -0500

    Updated header.php.

--- templates/header.php
+++ templates/header.php
@@ -4,7 +4,7 @@
     <meta http-equiv="content-type" content="text/html; charset=UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
-    <link href="/css/stylesheet.css?v=10.25.2022-1" rel="stylesheet">
+    <link href="/css/stylesheet.css?v=10.25.2022-2" rel="stylesheet">
     <link href="/css/font-awesome/font-awesome.min.css" rel="stylesheet">
     <?php if (isset($extraStylesheet)) {
         echo $extraStylesheet;
