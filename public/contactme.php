9cf8768f25e5389d316a96cfca7e99e99eb31f29
joco-88 <jocodigitalweb@gmail.com>
2022-10-26 21:08:56 +0000

    Oct 26, 2022, 4:08 PM

--- public/contactme.php
+++ public/contactme.php
@@ -7,7 +7,7 @@ include('/home/joco88.com/public_html/templates/header.php');
 <main>
   <div class="container-fluid">
       <br><h1>Welcome!</h1>
-      <p>On this page, I will test a basic contact form with validations.</p>
+      <p>On this page, I will test a basic contact form with validations. Also, I may add a captcha to prevent spam.</p>
   </div>
 </main>
   <footer>
