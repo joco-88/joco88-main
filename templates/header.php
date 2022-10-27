9d1fb59f5728a6039b85b09d7c6a7d56c3634b69
joco-88 <jocodigitalweb@gmail.com>
2022-10-27 21:54:55 +0000

    Oct 27, 2022, 4:54 PM

--- templates/header.php
+++ templates/header.php
@@ -25,7 +25,7 @@ $SiteName="Joco88";
     <header>
         <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
             <div class="container-fluid">
-            <a class="navbar-brand" href="/"><?php $SiteName; ?></a>
+            <a class="navbar-brand" href="/"> <?php $SiteName; ?> </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
               <span class="navbar-toggler-icon"></span>
             </button>
