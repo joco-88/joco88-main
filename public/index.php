244613f76a3862a34a55c29362b2d3b59b7f33ff
joco-88 <jocodigitalweb@gmail.com>
2022-10-27 22:02:58 +0000

    Oct 27, 2022, 5:02 PM

--- public/index.php
+++ public/index.php
@@ -15,7 +15,7 @@ include('../templates/header.php');
       </ol>
     <p><strong>10/25/2022</strong></p>
       <ol>
-        <li>Implemented first JavaScript.</li>
+        <li>Implemented first JavaScript / jquery script to website.</li>
         <li>Introduced Bootstrap and completed first Navbar with dropdown link integration..</li>   
     <p><strong>10/27/2022</strong></p>
       <ol>
