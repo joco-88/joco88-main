a6c4cede6c0c247b06bc9de44733a88d6c0e15e6
joco-88 <jocodigitalweb@gmail.com>
2022-10-27 21:53:42 +0000

    Oct 27, 2022, 4:53 PM

--- public/index.php
+++ public/index.php
@@ -12,7 +12,8 @@ include('../templates/header.php');
       <p><strong>10/27/2022</strong></p>
       <ol>
           <li>Added new "SiteName" variable instead of hardcoding throughout site.</li>
-          
+
+      <ol>
   </div>
 </main>
   <footer>
