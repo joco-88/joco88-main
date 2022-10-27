6228fc691759dcca321e8eedea0c14ac1fe227e9
joco-88 <jocodigitalweb@gmail.com>
2022-10-27 22:06:26 +0000

    Oct 27, 2022, 5:06 PM

--- public/index.php
+++ public/index.php
@@ -19,7 +19,8 @@ include('../templates/header.php');
         <li>Implemented FrontAwesome stylesheets and utilzied them in photoviewr.</li>
         <li>Successfully added photo viewer to website which allows for zooming and rotating of images</li>
             <ul><li>Added this under a new menu item (Projects)</li>
-                <li>Ported this to Webpoint production site with Dustin<</ul>   
+                <li>Ported this to Webpoint production site with Dustin</li>
+            </ul>   
     <p><strong>10/27/2022</strong></p>
       <ol>
         <li>Added new "SiteName" variable instead of hardcoding throughout site.</li>
