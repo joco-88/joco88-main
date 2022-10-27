69fc5f521878a888cc263d6b5bb191477fb7267c
joco-88 <jocodigitalweb@gmail.com>
2022-10-27 22:03:30 +0000

    Oct 27, 2022, 5:03 PM

--- public/index.php
+++ public/index.php
@@ -16,7 +16,8 @@ include('../templates/header.php');
     <p><strong>10/25/2022</strong></p>
       <ol>
         <li>Implemented first JavaScript / jquery script to website.</li>
-        <li>Implemented FrontAwesome stylesheets and utilzied them in photoviewr.</li>   
+        <li>Implemented FrontAwesome stylesheets and utilzied them in photoviewr.</li>
+        <li>   
     <p><strong>10/27/2022</strong></p>
       <ol>
         <li>Added new "SiteName" variable instead of hardcoding throughout site.</li>
