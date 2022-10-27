910f77fed19e59bb6052629b83c034e3bb9bb6ef
joco-88 <jocodigitalweb@gmail.com>
2022-10-27 22:03:58 +0000

    Oct 27, 2022, 5:03 PM

--- public/index.php
+++ public/index.php
@@ -17,7 +17,7 @@ include('../templates/header.php');
       <ol>
         <li>Implemented first JavaScript / jquery script to website.</li>
         <li>Implemented FrontAwesome stylesheets and utilzied them in photoviewr.</li>
-        <li>Successfully added photo viewer to website   
+        <li>Successfully added photo viewer to website which allows for zooming and rotating of images   
     <p><strong>10/27/2022</strong></p>
       <ol>
         <li>Added new "SiteName" variable instead of hardcoding throughout site.</li>
