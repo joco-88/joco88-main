6253e2efade444238aa8fe3d71a9674df6aa0108
joco-88 <jocodigitalweb@gmail.com>
2022-10-27 22:02:41 +0000

    Oct 27, 2022, 5:02 PM

--- public/index.php
+++ public/index.php
@@ -13,7 +13,7 @@ include('../templates/header.php');
         <li>Built out basic page framework and php file structure.</li>
         <li>Introduced Bootstrap and completed first Navbar with dropdown link integration..</li>
       </ol>
-    <p><strong>10/24/2022</strong></p>
+    <p><strong>10/25/2022</strong></p>
       <ol>
         <li>Built out basic page framework and php file structure.</li>
         <li>Introduced Bootstrap and completed first Navbar with dropdown link integration..</li>   
