4430ec0801e958441fb8361f044315453c4bb53c
joco-88 <jocodigitalweb@gmail.com>
2022-11-07 13:33:29 -0600

    Updated contactform.php.

--- public/includes/contactform.php
+++ public/includes/contactform.php
@@ -3,13 +3,14 @@ $PageTitle="Contact Form Submitted";
 include('../templates/header.php');
 ?>
 <main>
+<div class="container-fluid">
 Hello,&nbsp<?php echo $_POST["fname"]; ?>!<br><br>
 Thanks for reaching out.<br>
 Once we have reviewed your message, we will reach out to you at the email address you provided, which is: <strong><?php echo $_POST["email"]; ?></strong>
 <br>
 <br>
 Have a great day!<br>
-
+<div>
 </main>
 
 <!-- End of main content - footer will start below. -->
