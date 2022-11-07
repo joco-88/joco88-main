4b7e0f7311add18a3d73796953aef24045a89a15
joco-88 <jocodigitalweb@gmail.com>
2022-11-07 17:28:58 -0600

    Updated contact-action.php.

--- public/includes/contact-action.php
+++ public/includes/contact-action.php
@@ -15,7 +15,7 @@ $contactReason = $_POST['contact_reason'];
 	$email_subject = "Joco88 - New Form Submission";
 
 	$email_body = "You have received a new message from the user $fullName.\n".
-                            "Here is the message: <br> $customerMessage.\n".
+                            "Here is the message:\n $customerMessage.\n".
                             "The contact reason is:\n $contactReason.\n".
                             "The customer email is:\n $customerEmail.\n".
                             "The customer phone number is:\n $phone.\n";
