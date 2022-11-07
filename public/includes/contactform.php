4879b0a1454c31c2780fdc279bf5e1da8679250a
joco-88 <jocodigitalweb@gmail.com>
2022-11-07 13:01:54 -0600

    Updated contactform.php.

--- public/includes/contactform.php
+++ public/includes/contactform.php
@@ -1,12 +1,2 @@
-<html>
-<body>
-
-Thank you for reaching out, <?php echo $_POST["fname"]; ?><br>
-Once we have reviewed your message, we will reach out to you at your provided email address: <?php echo $_POST["email"]; ?>
-<br>
-<br>
-Thanks, and have a great day!<br>
-Joe C.
-
-</body>
-</html>
+<?php
+include '../app/events/contactform-event.php';
