3deff80927a7b4f86d1f0f2ecef40be815327128
joco-88 <jocodigitalweb@gmail.com>
2022-11-07 11:41:13 -0600

    Updated contactme.php.

--- public/contactme.php
+++ public/contactme.php
@@ -6,9 +6,10 @@ include('../templates/header.php');
 <!-- Body starts here as body tag opens at end of Header.php file -->
 <main>
   <div class="container-fluid">
-      <br><h1>Contact Me.</h1>
+      <br><h1>Contact Me</h1>
       <p>On this page, I will test a basic contact form with validations. Might try bootstrap styling.</p>
       <p> Also, I may add a captcha field to prevent more spam. Test</p>
+      <hr>
   </div>
 
 <!-- Contact form will go below this -->
