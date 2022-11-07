c24547bbbdd3bbe4768bbd13c0bf8810eaf31dfa
joco-88 <jocodigitalweb@gmail.com>
2022-11-07 13:52:50 -0600

    Updated contactform.php.

--- public/includes/contactform.php
+++ public/includes/contactform.php
@@ -2,8 +2,24 @@
 $PageTitle="Contact Form Submitted";
 include ('../templates/header.php');
 ?>
+
 <?php echo "Testing"; ?>
+
 <main>
+
+  <?php
+  if(isset($_POST['submit']))
+  {
+      $name = $_POST['fname'];
+      echo "User Has submitted the form and entered this name : <b> $fname </b>";
+      echo "<br>You can use the following form again to enter a new name.";
+  }
+  ?>
+  <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+     <input type="text" name="name"><br>
+     <input type="submit" name="submit" value="Submit Form"><br>
+  </form>
+
 <div class="container-fluid">
 Hello,&nbsp<?php echo $_POST["fname"]; ?>!<br><br>
 Thanks for reaching out.<br>
@@ -13,6 +29,8 @@ Once I have reviewed your message, we will reach out to you at the email address
 Have a great day!<br>
 <div>
 </main>
+
+
 
 <!-- End of main content - footer will start below. -->
 
