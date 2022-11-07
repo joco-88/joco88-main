1975b6e1385c7f27695041966c270a3493525784
joco-88 <jocodigitalweb@gmail.com>
2022-11-07 14:02:12 -0600

    Updated contactform.php.

--- public/includes/contactform.php
+++ public/includes/contactform.php
@@ -5,6 +5,22 @@ include('../templates/header.php');
 ?>
 
 
+<!doctype html>
+<html lang="en">
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <link href="/css/stylesheet.css?v=10.25.2022.4" rel="stylesheet">
+    <link href="/css/font-awesome/font-awesome.min.css" rel="stylesheet">
+    <?php if (isset($extraStylesheet)) {
+        echo $extraStylesheet;
+      }
+$SiteName="Joco88";
+    ?>
+
+
+
 <header>
     <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
         <div class="container-fluid">
