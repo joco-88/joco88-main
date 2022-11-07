2595ca69546229136f3aecc60e1f6e7e1c3f19ea
joco-88 <jocodigitalweb@gmail.com>
2022-11-07 13:55:43 -0600

    Updated contactform.php.

--- public/includes/contactform.php
+++ public/includes/contactform.php
@@ -1,24 +1,11 @@
 <?php
 $PageTitle="Contact Form Submitted";
 include ('../templates/header.php');
-?>
 
-<?php echo "Testing"; ?>
+
+echo "Testing"; ?>
 
 <main>
-
-  <?php
-  if(isset($_POST['submit']))
-  {
-      $name = $_POST['fname'];
-      echo "User Has submitted the form and entered this name : <b> $fname </b>";
-      echo "<br>You can use the following form again to enter a new name.";
-  }
-  ?>
-  <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
-     <input type="text" name="name"><br>
-     <input type="submit" name="submit" value="Submit Form"><br>
-  </form>
 
 <div class="container-fluid">
 Hello,&nbsp<?php echo $_POST["fname"]; ?>!<br><br>
