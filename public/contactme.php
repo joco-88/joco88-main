b2ecf202a327008e5a87acef96c191e587ee6611
joco-88 <jocodigitalweb@gmail.com>
2022-11-07 11:19:50 -0600

    Updated contactme.php.

--- public/contactme.php
+++ public/contactme.php
@@ -24,7 +24,7 @@ include('../templates/header.php');
         <label for="email">Email:</label><br>
         <input type="email" id="email" name="email" required="true"><br>
         <label for="phone">Phone:</label><br>
-        <input type="number" id="phone" name="phone" required="false"><br>
+        <input type="number" id="phone" name="phone" required="false" min="10"><br>
       </fieldset>
       <fieldset>
         <legend><br>Additional Info:</legend>
