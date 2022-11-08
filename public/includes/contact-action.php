e01a6856dd07be62eebcd31d0be5865fe74eeccc
joco-88 <jocodigitalweb@gmail.com>
2022-11-08 17:21:26 -0600

    Updated contact-action.php.

--- public/includes/contact-action.php
+++ public/includes/contact-action.php
@@ -103,21 +103,9 @@ Once we have reviewed your message, we will reach out to you at the email addres
 <br>
 <br>
 Have a great day!<br><hr>
-<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
-    <div class="container-fluid">
-    <!--<a class="navbar-brand" href="/"> <?php echo $SiteName; ?> </a> -->
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="collapsibleNavbar">
-      <ul class="navbar-nav">
-      <li class="nav-item">
-        <a class="nav-link" href="/"><center>&#x2190;&nbspGo Back to the Home Page</center></a>
-    </li>
-  </ul>
-</div>
-</div>
-</nav>
+
+
+
 </center>
 <div>
 </main>
