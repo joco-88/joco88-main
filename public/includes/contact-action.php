fa75b9b0f483774b52570b70b155f12810869a24
joco-88 <jocodigitalweb@gmail.com>
2022-11-08 16:24:00 -0600

    Updated contact-action.php.

--- public/includes/contact-action.php
+++ public/includes/contact-action.php
@@ -29,6 +29,8 @@ $SiteName="Joco88";
               <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
                 <span class="navbar-toggler-icon"></span>
               </button>
+              <div class="collapse navbar-collapse" id="collapsibleNavbar">
+                <ul class="navbar-nav">
                 <li class="nav-item">
                   <a class="nav-link" href="/index.php">Go Back to the Home Page</a>
               </li>
