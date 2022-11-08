7d1a5f5c71cca4f41ad5275366aafe37ffd04154
joco-88 <jocodigitalweb@gmail.com>
2022-11-08 16:19:34 -0600

    Updated contact-action.php.

--- public/includes/contact-action.php
+++ public/includes/contact-action.php
@@ -131,7 +131,11 @@ Have a great day!<br><hr>
 }
 ?>
 
+<!-- The Below code is simply testing if variables are set, but shoudln't show to users -->
+
 <?php
+/*
+
 echo "<strong>First Name:</strong> $fname <br>";
 echo "<strong>Last Name:</strong> $lname <br>";
 echo "<strong>Full Name:</strong> $fullName <br>";
@@ -144,15 +148,27 @@ echo "<strong>Email Subject is:</strong> $email_subject <br>";
 echo "<strong>Email Headers are:</strong> $headers <br>";
 echo "<strong>Email full body is:</strong><br> $email_body <br>";
 
- ?>
+  */ ?>
 
 
 <!-- End of main content - footer will start below. -->
 
   <footer>
-    <?php
-      include('../templates/footer.php');
-    ?>
+
+    <!-- JavaScript Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.min.js" integrity="sha384-IDwe1+LCz02ROU9k972gdyvl+AESN10+x7tBKgc9I5HFtuNz0wWnPclzo6p9vxnk" crossorigin="anonymous"></script>
+
+    <!--- Start Footer Content --->
+
+    <center>
+      <div class="footerLinks">
+      <div class="container-fluid p-5 my-5 bg-secondary text-white">
+        &copy;<?php echo date("Y");?> - <a href="/contactme.php"><strong>Joseph Condon</strong></a> & <a href="/"><strong>Joco88.com</strong></a></center>
+      </div>
+      </div>
+
+
   </footer>
 </body>
 </html>
