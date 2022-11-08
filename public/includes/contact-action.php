133244158001a6a09db91bcb68c56486eee7bee0
joco-88 <jocodigitalweb@gmail.com>
2022-11-07 18:56:19 -0600

    Updated contact-action.php.

--- public/includes/contact-action.php
+++ public/includes/contact-action.php
@@ -36,7 +36,7 @@ Hello,&nbsp
 <?php echo $_POST["fname"]; ?>
 !<br><br>
 Thanks for reaching out.<br>
-Once I have reviewed your message, we will reach out to you at the email address you provided, which is:
+Once we have reviewed your message, we will reach out to you at the email address you provided, which is:
 <strong>
   <?php echo $_POST["email"]; ?>
 </strong>
