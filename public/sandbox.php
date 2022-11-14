291c3a4d2875c003ad7a1322e7ad5cfc8d0d4d1b
joco-88 <jocodigitalweb@gmail.com>
2022-11-14 17:52:31 -0600

    Updated sandbox.php.

---
+++ public/sandbox.php
@@ -0,0 +1,31 @@
+<?php
+$PageTitle="Home";
+include('../templates/header.php');
+?>
+
+  <main>
+    <div class="container-fluid">
+        <br><h1>Get In Touch</h1>
+        <p>Have questions or want to reach out? Simply fill out the contact form below and we will get back to you the second we have time.</p>
+        <hr>
+    </div>
+
+<!-- Contact form will go below this -->
+
+
+
+
+
+
+
+
+<!-- End of Contact Form -->
+
+</main>
+  <footer>
+    <?php
+      include('../templates/footer.php');
+    ?>
+  </footer>
+</body>
+</html>
