7ed79270be33611e8c98cafec0df2c69051ff3c6
joco-88 <jocodigitalweb@gmail.com>
2022-11-15 11:19:37 -0600

    Updated sandbox.php.

--- public/sandbox.php
+++ public/sandbox.php
@@ -45,6 +45,16 @@ include('../templates/header.php');
   </div>
 </div>
 
+<!-- Vertically centered modal -->
+<div class="modal-dialog modal-dialog-centered">
+  ...
+</div>
+
+<!-- Vertically centered scrollable modal -->
+<div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
+  ...
+</div>
+
 
 <!-- End of Contact Form -->
 
