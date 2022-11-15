ba36697c28f276d396e5a5ac0cbe537647f4eeea
joco-88 <jocodigitalweb@gmail.com>
2022-11-15 10:42:48 -0600

    Updated header.php.

--- templates/header.php
+++ templates/header.php
@@ -18,9 +18,7 @@ $SiteName="Joco88";
 
 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.4.1/dist/css/bootstrap-theme.min.css" integrity="sha384-6pzBo3FDv/PJ8r2KRkGHifhEocL+1X2rVCTTkUfGk7/0pbek5mMa1upzvWbrUbOZ" crossorigin="anonymous">
-
-
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap-theme.min.css">
 
     <link rel="stylesheet" href="/css/stylesheet.css?v=11.15.2022.1">
     <link rel="stylesheet" href="/css/font-awesome/font-awesome.min.css">
