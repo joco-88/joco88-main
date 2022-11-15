af822f93c25dd12c22ed8d5e0f740662d9dfa362
joco-88 <jocodigitalweb@gmail.com>
2022-11-15 16:03:32 -0600

    Updated sandbox.php.

--- public/sandbox.php
+++ public/sandbox.php
@@ -67,9 +67,24 @@ include('../templates/header.php');
                      </div>
 
             <div class="modal-footer d-flex justify-content-center">
-              <input type="submit" name"submit" class="btn btn-primary btn-send mb-2 btn-block" disabled value="Send Message"> 
+              <input type="submit" name"submit" class="btn btn-primary btn-send mb-2 btn-block" disabled value="Send Message">
 </form>
-				<!-- <input type="submit" name="submit" class="btn btn-primary btn-send mb-2 btn-block" value="Send Message"> -->
+
+<script>
+$('#user_input, #pass_input, #v_pass_input, #email').bind('keyup', function() {
+    if(allFilled()) $('#register').removeAttr('disabled');
+});
+
+function allFilled() {
+    var filled = true;
+    $('body input').each(function() {
+        if($(this).val() == '') filled = false;
+    });
+    return filled;
+}
+</script>
+
+
               </div>
 
 
