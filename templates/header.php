390df743277e450767bbda13d5841e925f3f47fe
joco-88 <jocodigitalweb@gmail.com>
2022-11-15 10:15:07 -0600

    Updated header.php.

--- templates/header.php
+++ templates/header.php
@@ -23,7 +23,7 @@ $SiteName="Joco88";
 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.4.1/dist/css/bootstrap.min.css" integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.4.1/dist/css/bootstrap-theme.min.css" integrity="sha384-6pzBo3FDv/PJ8r2KRkGHifhEocL+1X2rVCTTkUfGk7/0pbek5mMa1upzvWbrUbOZ" crossorigin="anonymous">
-    <link href="/css/stylesheet.css?v=11.09.2022.1" rel="stylesheet">
+    <link href="/css/stylesheet.css?v=11.15.2022.1" rel="stylesheet">
     <link href="/css/font-awesome/font-awesome.min.css" rel="stylesheet">
 
     <!-- JavaScript Bundle with Bootstrap Popper -->
