a4cfc461ed642d85c9970cf8b59d95faeb14f146
joco-88 <jocodigitalweb@gmail.com>
2022-11-15 10:25:18 -0600

    Updated header.php.

--- templates/header.php
+++ templates/header.php
@@ -9,15 +9,12 @@
 $SiteName="Joco88";
 ?>
 
-    <!--- This is my Font Awesome Kit I made. Right now, just using hosted (free) fonts and don't need to use it.
-    <script src="https://kit.fontawesome.com/347bff65d7.js" crossorigin="anonymous"></script>
-    --->
+
 
     <title>
     <?php echo isset($PageTitle) ? $PageTitle . " | $SiteName.com" : "$SiteName.com"?>
     </title>
     <link rel="icon" type="image/x-icon" href="/media/favicon.ico">
-
 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.4.1/dist/css/bootstrap.min.css" integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.4.1/dist/css/bootstrap-theme.min.css" integrity="sha384-6pzBo3FDv/PJ8r2KRkGHifhEocL+1X2rVCTTkUfGk7/0pbek5mMa1upzvWbrUbOZ" crossorigin="anonymous">
