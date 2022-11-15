177f34c22c51489a824046c880fe474cadcc72fa
joco-88 <jocodigitalweb@gmail.com>
2022-11-15 13:01:03 -0600

    Updated modalcontact-handler.php.

--- public/includes/modalcontact-handler.php
+++ public/includes/modalcontact-handler.php
@@ -1,6 +1,21 @@
 <?php
-   if($_POST["fullName"])
-   {
+
+$fullName = htmlspecialchars($_POST['fullName']);
+//$customerMessage = htmlspecialchars($_POST['customer_message']);
+$customerEmail = htmlspecialchars($_POST['email']);
+
+if (isset($_POST['customer_message'])) {
+  $customerMessage = htmlspecialchars($_POST['customer_message']);
+} else {
+  $customerMessage=NULL;
+}
+?>
+
+<?php
+
+<?php
+<?php
+    if (isset($customerMessage)) {
 
         echo "<h4>Thank you for submission!<h4>";
 		echo "<br>Your Name: <b>".$_POST["fullName"]."</b>";
