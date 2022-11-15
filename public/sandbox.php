05f0cc3601591fb0c8d6886293bad51947aff208
joco-88 <jocodigitalweb@gmail.com>
2022-11-15 13:42:20 -0600

    Updated sandbox.php.

--- public/sandbox.php
+++ public/sandbox.php
@@ -120,7 +120,7 @@ include('../templates/header.php');
             <div class="col-md-12">
             <br>
             <center>
-            <input type="submit" name="submit" class="btn btn-success btn-send  pt-2 btn-block" value="Send Message" >
+            <input type="submit" name="submit" class="btn btn-success btn-send  pt-2 btn-block" value="Send Message 2" >
             </center>
             </div>
             </div>
