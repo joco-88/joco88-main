91ce1b726c1fe709bddd4692cf2c94a77bb7c048
joco-88 <jocodigitalweb@gmail.com>
2022-11-15 20:40:55 -0600

    Updated testhandler.php.

--- public/includes/testhandler.php
+++ public/includes/testhandler.php
@@ -15,22 +15,22 @@ if(isset($_POST['contactFrmSubmit']))
 
 
 
-    $customerName = $_POST['name'];
-    $customerEmail = $_POST['email'];
-    $customerMessage = $_POST['message'];
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
 
-    $emailTo = "jocodigitalweb@gmail.com";
+    $to = "jocodigitalweb@gmail.com";
     $email_from = "<mail@joco88.com>";
     $email_subject= "Contact Request Submitted";
 
-    $email_body = "<b>You have received a new message from the following user:&nbsp</b> $customerName<br><br>".
-                              "<b>Here is the message:</b><br><br><q><em>$customerMessage</em></q><br><br>".
-                              "<b>The customer email is:</b>  $customerEmail<br><br>";
+    $email_body = "<b>You have received a new message from the following user:&nbsp</b> $name<br><br>".
+                              "<b>Here is the message:</b><br><br><q><em>$message</em></q><br><br>".
+                              "<b>The customer email is:</b>  $email<br><br>";
 
     $headers = "Content-type: text/html; charset=iso-8859-1 \r\n";
     $headers .= "From: Joco88.com Contact Page Submission $email_from \r\n";
-    $headers .= "Reply-To: $customerEmail \r\n";
-    mail($emailTo,$email_subject,$email_body,$headers);
+    $headers .= "Reply-To: $email \r\n";
+    mail($to,$email_subject,$email_body,$headers);
     $status = 'ok';
     echo $status;
 } else {
