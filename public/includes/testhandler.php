e0f1eb2f982e0b92fc538dcdfb5d8890bc744924
joco-88 <jocodigitalweb@gmail.com>
2022-11-15 18:59:54 -0600

    Updated testhandler.php.

--- public/includes/testhandler.php
+++ public/includes/testhandler.php
@@ -27,8 +27,9 @@ if(isset($_POST['contactFrmSubmit']) && !empty($_POST['name']) && !empty($_POST[
     </table>';
 
     // Set content-type header for sending HTML email
-    $headers = "MIME-Version: 1.0" . "\r\n";
-    $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+    $headers = "Content-type: text/html; charset=iso-8859-1 \r\n";
+    $headers .= "From: Joco88.com Contact Page Submission $email_from \r\n";
+    $headers .= "Reply-To: $customerEmail \r\n";
 
     // Additional headers
     $headers .= 'From: Joco88<mail@joco88.com>' . "\r\n";
