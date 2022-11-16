b256f3619202c2843ff4672c6d78b487da273413
joco-88 <jocodigitalweb@gmail.com>
2022-11-15 18:16:14 -0600

    Updated sandbox.php.

--- public/sandbox.php
+++ public/sandbox.php
@@ -99,10 +99,7 @@ $('#modalContactForm').modal('hide');
 
 
 
-<!-- Button to trigger modal -->
-<button class="btn btn-success btn-lg" data-toggle="modal" data-target="#modalForm">
-    Open Contact Form
-</button>
+
 
 <!-- Modal -->
 <div class="modal fade" id="modalForm" role="dialog">
@@ -146,7 +143,10 @@ $('#modalContactForm').modal('hide');
 </div>
 
 
-
+<!-- Button to trigger modal -->
+<button class="btn btn-success btn-lg" data-toggle="modal" data-target="#modalForm">
+    Open Contact Form
+</button>
 
 <!--  END OF NEW MODAL ------------------------------------->
 
