032cb13c07c07a5ebb77c477dee9f3b0d269bd12
joco-88 <jocodigitalweb@gmail.com>
2022-11-16 08:58:33 -0600

    Updated sandbox.php.

--- public/sandbox.php
+++ public/sandbox.php
@@ -116,10 +116,7 @@ $('#modalContactForm').modal('hide');
         <div class="modal-content">
             <!-- Modal Header -->
             <div class="modal-header">
-                <button type="button" class="btn-close" data-bs-dismiss="modal">
-                    <span aria-hidden="true">&times;</span>
-                    <span class="sr-only">Close</span>
-                </button>
+                <button type="button" class="btn-close" aria-label="Close"></button>
                 <h4 align="left" class="modal-title" id="myModalLabel">Contact Form</h4>
             </div>
 
