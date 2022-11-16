5d0b8ec7ecfa452316f40208a0fdd768f07c047f
joco-88 <jocodigitalweb@gmail.com>
2022-11-15 20:43:33 -0600

    Updated testhandler.php.

--- public/includes/testhandler.php
+++ public/includes/testhandler.php
@@ -1,34 +1,45 @@
 <?php
+if(isset($_POST['contactFrmSubmit']) && !empty($_POST['name']) && !empty($_POST['email']) && (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL) === false) && !empty($_POST['message'])){
 
+    // Submitted form data
+    $name   = $_POST['name'];
+    $email  = $_POST['email'];
+    $message= $_POST['message'];
 
-if(isset($_POST['contactFrmSubmit']))
-    && !empty($_POST['name'])
-    && !empty($_POST['email'])
-    && (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL) === false)
-    && !empty($_POST['message']))
-{
+    /*
+     * Send email to admin
+     */
+    $to     = 'admin@example.com';
+    $subject= 'Contact Request Submitted';
 
+    $htmlContent = '
+    <h4>Contact request has submitted at CodexWorld, details are given below.</h4>
+    <table cellspacing="0" style="width: 300px; height: 200px;">
+        <tr>
+            <th>Name:</th><td>'.$name.'</td>
+        </tr>
+        <tr style="background-color: #e0e0e0;">
+            <th>Email:</th><td>'.$email.'</td>
+        </tr>
+        <tr>
+            <th>Message:</th><td>'.$message.'</td>
+        </tr>
+    </table>';
 
+    // Set content-type header for sending HTML email
+    $headers = "MIME-Version: 1.0" . "\r\n";
+    $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
 
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $message = $_POST['message'];
+    // Additional headers
+    $headers .= 'From: CodexWorld<sender@example.com>' . "\r\n";
 
-    $to = "jocodigitalweb@gmail.com";
-    $email_from = "<mail@joco88.com>";
-    $email_subject= "Contact Request Submitted";
+    // Send email
+    if(mail($to,$subject,$htmlContent,$headers)){
+        $status = 'ok';
+    }else{
+        $status = 'err';
+    }
 
-    $email_body = "<b>You have received a new message from the following user:&nbsp</b> $name<br><br>".
-                              "<b>Here is the message:</b><br><br><q><em>$message</em></q><br><br>".
-                              "<b>The customer email is:</b>  $email<br><br>";
-
-    $headers = "Content-type: text/html; charset=iso-8859-1 \r\n";
-    $headers .= "From: Joco88.com Contact Page Submission $email_from \r\n";
-    $headers .= "Reply-To: $email \r\n";
-    mail($to,$email_subject,$email_body,$headers);
-    $status = 'ok';
-    echo $status;
-} else {
-    echo "err";
+    // Output status
+    echo $status;die;
 }
-  echo $status;die;
