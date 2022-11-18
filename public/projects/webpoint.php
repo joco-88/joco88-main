1563c54574004d384d6416d93fdf9c8de5baa13e
joco-88 <jocodigitalweb@gmail.com>
2022-11-18 13:15:12 -0600

    Updated webpoint.php.

--- public/projects/webpoint.php
+++ public/projects/webpoint.php
@@ -9,12 +9,23 @@ error_reporting(E_ALL); ini_set('display_errors', 1);
 
 
 
-    <!-- Scrollable modal -->
-    <div class="modal-dialog modal-dialog-scrollable">
-      <p>Test paragraph</p>
+    <div class="modal" tabindex="-1">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title">Modal title</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          </div>
+          <div class="modal-body">
+            <p>Modal body text goes here.</p>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+            <button type="button" class="btn btn-primary">Save changes</button>
+          </div>
+        </div>
+      </div>
     </div>
-
-
 
 
 
