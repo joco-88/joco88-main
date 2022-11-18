1d3f82b70e95d219703631ceb9c3cf39d3ea49d9
joco-88 <jocodigitalweb@gmail.com>
2022-11-18 05:42:25 +0000

    Nov 17, 2022, 11:42 PM

--- public/projects/webpoint.php
+++ public/projects/webpoint.php
@@ -1,7 +1,6 @@
 <?php
 $PageTitle="Home";
-include('../templates/header.php');
-
+include('../../templates/header.php');
 error_reporting(E_ALL); ini_set('display_errors', 1);
 
 ?>
@@ -20,7 +19,7 @@ error_reporting(E_ALL); ini_set('display_errors', 1);
 <!--  Any necessary scripts go below here --------------------------->
 
     <?php
-      include('../templates/footer.php');
+      include('../../templates/footer.php');
     ?>
   </footer>
 </body>
