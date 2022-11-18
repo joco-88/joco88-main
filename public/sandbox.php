aee0faec9f2b8b50dfd717e051b33c01f3c82938
joco-88 <jocodigitalweb@gmail.com>
2022-11-18 13:19:28 -0600

    Updated sandbox.php.

--- public/sandbox.php
+++ public/sandbox.php
@@ -15,19 +15,25 @@ error_reporting(E_ALL); ini_set('display_errors', 1);
 <!-- Upcoming New Sandbox Project Will Go Below -->
 
 
+<!-- Button trigger modal -->
+<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+  Launch Test Modal
+</button>
+
+<!-- Modal -->
+
 <div class="modal" tabindex="-1">
   <div class="modal-dialog">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title">Modal title</h5>
+        <h5 class="modal-title">Event Schedule</h5>
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body">
-        <p>Modal body text goes here.</p>
+        <p>Event Info and Schedulue is showing here.</p>
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
       </div>
     </div>
   </div>
