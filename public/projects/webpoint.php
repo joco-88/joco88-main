091cfebc42149e3ec51f3266fa4ae7d72e9dae82
joco-88 <jocodigitalweb@gmail.com>
2022-11-18 05:40:22 +0000

    Nov 17, 2022, 11:40 PM

---
+++ public/projects/webpoint.php
@@ -0,0 +1,27 @@
+<?php
+$PageTitle="Home";
+include('../templates/header.php');
+
+error_reporting(E_ALL); ini_set('display_errors', 1);
+
+?>
+  <main>
+    <div class="container-fluid">
+        <br><h1>Sandbox Page</h1>
+        <p>On this page, I will test new projects and hone my coding knowledge.</p>
+        <hr>
+    </div>
+
+<!-- Upcoming New Sandbox Project Will Go Below -->
+
+
+</main>
+  <footer>
+<!--  Any necessary scripts go below here --------------------------->
+
+    <?php
+      include('../templates/footer.php');
+    ?>
+  </footer>
+</body>
+</html>
