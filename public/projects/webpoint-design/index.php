45aa3ffc0c5c92fcb58f70c52d35177f3eaea3e8
joco-88 <jocodigitalweb@gmail.com>
2022-11-21 13:08:49 -0600

    Updated index.php.

--- public/projects/webpoint-design/index.php
+++ public/projects/webpoint-design/index.php
@@ -240,13 +240,6 @@
 
           <div class="col-lg-3 col-md-6 footer-contact">
             <h3>Webpoint</h3>
-            <p>
-              A108 Adam Street <br>
-              New York, NY 535022<br>
-              United States <br><br>
-              <strong>Phone:</strong> +1 5589 55488 55<br>
-              <strong>Email:</strong> info@example.com<br>
-            </p>
           </div>
 
           <div class="col-lg-3 col-md-6 footer-links">
