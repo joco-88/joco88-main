3c625b8e030de9ea699ed4159ab94271cc6d8eab
joco-88 <jocodigitalweb@gmail.com>
2022-11-21 11:44:42 -0600

    Updated index.php.

--- public/projects/webpoint-design/index.php
+++ public/projects/webpoint-design/index.php
@@ -58,13 +58,13 @@
       <div class="row">
         <div class="col-lg-6 d-flex flex-column justify-content-center pt-4 pt-lg-0 order-2 order-lg-1" data-aos="fade-up" data-aos-delay="200">
           <h1>Experts in National Sports Organization Management Systems</h1>
-          <h3>For over two decades our team has been serving the US Olympic NGB movement with highly customized integrated membership, certification and event management platforms. Giving you the tools to manage your organization at the national, regional and local level, allowing you to focus on growing your sport.</h3>
-          <div class="d-flex justify-content-center justify-content-lg-start">
+          <h4>For over two decades our team has been serving the US Olympic NGB movement with highly customized integrated membership, certification and event management platforms.
+            Giving you the tools to manage your organization at the national, regional and local level, allowing you to focus on growing your sport.</h4>          <div class="d-flex justify-content-center justify-content-lg-start">
             <a href="#about" class="btn-get-started scrollto">Our Services</a>
           </div>
         </div>
         <div class="col-lg-6 order-1 order-lg-2 hero-img" data-aos="zoom-in" data-aos-delay="200">
-          <img src="assets/img/seatsbackground.jpg" class="img-fluid animated" alt="">
+          <img src="assets/img/hero-img.png" class="img-fluid animated" alt="">
         </div>
       </div>
     </div>
