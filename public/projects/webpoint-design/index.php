8314834d5923dc1f6e91f58a7ee116b6238238d7
joco-88 <jocodigitalweb@gmail.com>
2022-11-21 13:48:36 -0600

    Updated index.php.

--- public/projects/webpoint-design/index.php
+++ public/projects/webpoint-design/index.php
@@ -43,7 +43,7 @@
       <nav id="navbar" class="navbar">
         <ul>
           <li><a class="nav-link scrollto" href="#services">Services</a></li>
-          <li><a class="getstarted scrollto" href="#about">Contact Us</a></li>
+          <li><a class="getstarted scrollto" href="#contact">Contact Us</a></li>
         </ul>
         <i class="bi bi-list mobile-nav-toggle"></i>
       </nav><!-- .navbar -->
