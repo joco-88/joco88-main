02c5c84c8b283dddec2db4e104d65005db132232
joco-88 <jocodigitalweb@gmail.com>
2022-11-21 13:52:08 -0600

    Updated index.php.

--- public/projects/webpoint-design/index.php
+++ public/projects/webpoint-design/index.php
@@ -219,11 +219,11 @@
             <p> Simply click the button below to fill out our contact form. <br>
               Reach out to us with any questions you may have or to setup a time to discuss your needs. We will reply as soon as possible.</p>
           </div>
-
+</div>
 <a href="#modalForm" class="btn btn-primary btn-lg" data-bs-toggle="modal" data-target="#modalForm">Contact Us Today!</a>
 
 
-        </div>
+
 
       </div>
     </section><!-- End Contact Section -->
