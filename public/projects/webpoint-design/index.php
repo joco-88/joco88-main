c1876c4fa07dc6aca6b93e5492cf2e15fc63fbb7
joco-88 <jocodigitalweb@gmail.com>
2022-11-21 12:43:31 -0600

    Updated index.php.

--- public/projects/webpoint-design/index.php
+++ public/projects/webpoint-design/index.php
@@ -112,7 +112,7 @@
            </div>
 
            <div class="row content">
-             <div class="col-lg-6">
+        
                <p>For over two decades our team has been serving the US Olympic NGB movement with highly customized integrated membership, certification and event
                  management platforms. Giving you the tools to manage your organization at the
                   national, regional and local level, allowing you to focus on growing your sport.
