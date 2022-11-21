c015016ee1167a7c68d30930fbe47a56708793b3
joco-88 <jocodigitalweb@gmail.com>
2022-11-21 15:31:16 -0600

    Updated index.php.

--- public/projects/webpoint-design/index.php
+++ public/projects/webpoint-design/index.php
@@ -234,7 +234,7 @@
             <div class="modal-content">
               <!--   Modal Header -->
                  <div class="modal-header">
-                   <center><img src="assets/img/webpoint.gif" alt="" width="50"></center><br>
+                   <center><img src="assets/img/webpoint.gif" alt="" width="150"></center><br><br>
                     <h4 class="modal-title">Contact Us</h4>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
