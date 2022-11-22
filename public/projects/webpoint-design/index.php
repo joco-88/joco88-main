68f59f4bd4231bf56deeb9b56a003aa1b271283d
joco-88 <jocodigitalweb@gmail.com>
2022-11-21 19:48:51 -0600

    Updated index.php.

--- public/projects/webpoint-design/index.php
+++ public/projects/webpoint-design/index.php
@@ -26,7 +26,7 @@
   <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
 
   <!-- Template Main CSS File -->
-  <link href="assets/css/style.css?v=11.21.2022.20" rel="stylesheet">
+  <link href="assets/css/style.css?v=11.21.2022.21" rel="stylesheet">
 
 </head>
 
