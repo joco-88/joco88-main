3725bd6fd04832bf48637942acad6a350003f4a1
joco-88 <jocodigitalweb@gmail.com>
2022-11-29 14:12:12 -0600

    Updated PROJECT-TEMPLATE.php.

---
+++ public/projects/PROJECT-TEMPLATE.php
@@ -0,0 +1,31 @@
+<?php
+$PageTitle="Test Write to PHP File";
+include('../../templates/header.php');
+
+error_reporting(E_ALL); ini_set('display_errors', 1);
+
+?>
+  <main>
+    <div class="container-fluid">
+        <br><h1>Write To File</h1>
+        <p>On this page, I will test new projects and hone my basic coding knowledge.</p>
+        <hr>
+    </div>
+
+<!-- Upcoming New Sandbox Project Will Go Below -->
+
+
+
+
+
+
+</main>
+  <footer>
+<!--  Any necessary scripts go below here --------------------------->
+
+    <?php
+      include('../../templates/footer.php');
+    ?>
+  </footer>
+</body>
+</html>
