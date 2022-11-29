22f3caf71cc641ad2b87567a60e5be8b57320de9
joco-88 <jocodigitalweb@gmail.com>
2022-11-29 14:55:32 -0600

    Updated header.php.

--- templates/header.php
+++ templates/header.php
@@ -46,7 +46,7 @@ $SiteName="Joco88";
                 <ul class="dropdown-menu">
                   <li><a class="dropdown-item" href="/projects/photoviewer.php">Photo Viewer</a></li>
                   <li><a class="dropdown-item" href="/projects/contactmodal.php">Contact Modal</a></li>
-                  <li><a class="dropdown-item" href="/projects/jocoreturnform.php">Joco Return Form</a></li>
+                  <li><a class="dropdown-item" href="/projects/jocoreturnform.php">Joco Returns</a></li>
                   <li><a class="dropdown-item" href="/projects/webpoint-design">Webpoint Re-design</a></li>
                 </ul>
               </li>
