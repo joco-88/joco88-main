332db2afa898b410132abd9a1d80178c5920e72a
joco-88 <jocodigitalweb@gmail.com>
2022-12-09 10:23:01 -0600

    Updated sandbox.php.

--- public/sandbox.php
+++ public/sandbox.php
@@ -14,8 +14,466 @@ error_reporting(E_ALL); ini_set('display_errors', 1);
 
 <!-- Upcoming New Sandbox Project Will Go Below -->
 
+<?php
+
+// Setting Initial Variables so we can define them on each event page, then dynamically insert them into the sql connections. Not sure if this is best way to do this or not.
+$gameid = 117451;
+$player1_Id = 666618;
+$player2_Id = 578652;
+$wpClient = "USA Boxing";
 
 
+
+//Game Data Variables
+$eventId = 11111;
+$eventTitle = "The Best Fight Ever!"";
+$eventDescription = "This fight is for warriors. Man, they are really going to fuck each other up.";
+$eventCategory = "No Fatties Allowed";
+$eventStart = "12.10.22";
+$eventEnd = "12.12.2022";
+
+//Player 1 Variables
+$player1_name = $row['P1_Name'];
+$player1_Id = $row['P1_ContactID'];
+$player1_gender = $row['P1_Gender'];
+$player1_wins = $row2['GMWinsCount'];
+$player1_losses = $row2['GMLossCount'];
+$player1_experience = $player1_wins + $player1_losses . " Games";
+
+//Player 1 Conditional Variables
+    //Player 1 Organization Name
+        if (isset($row4['OtherOrgName'])) {
+            $player1_org = $row4['OtherOrgName'];
+        } else {
+            $player1_org = "Unaffiliated";
+        }
+
+    //Player 1 Date of Birth
+    if (isset($row4['BirthDate'])) {
+        $player1_birth = $row4['BirthDate']->format('m/d/Y');
+    } else {
+        $player1_birth = "Not Provided";
+    }
+
+    //Player 1 Age
+        if (isset($row4['Age'])) {
+            $player1_age = $row4['Age'];
+        } else {
+            $player1_age = "Not Provided";
+        }
+
+
+//Player 2 Variables
+$player2_name = $row['P2_Name'];
+$player2_Id = $row['P2_ContactID'];
+$player2_gender = $row['P2_Gender'];
+$player2_wins = $row3['GMWinsCount'];
+$player2_losses = $row3['GMLossCount'];
+$player2_experience = $player2_wins + $player2_losses . " Games";
+
+//Player 2 Conditional Variables
+    //Player 2 Organization Name
+        if (isset($row5['OtherOrgName'])) {
+            $player2_org = $row5['OtherOrgName'];
+        } else {
+            $player2_org = "Unaffiliated";
+        }
+
+    //Player 2 Date of Birth
+    if (isset($row5['BirthDate'])) {
+        $player2_birth = $row5['BirthDate']->format('m/d/Y');
+    } else {
+        $player2_birth = "Not Provided";
+    }
+
+    //Player 2 Age
+        if (isset($row5['Age'])) {
+            $player2_age = $row5['Age'];
+        } else {
+            $player2_age = "Not Provided";
+        }
+
+?>
+
+
+
+
+<!-- -------- START DISPLAYING COLLAPSIBLE TEST ECHOS --------- -->
+<center>
+    <br>
+<p>
+  <a class="btn btn-primary" data-bs-toggle="collapse" href="#collapse1" role="button" aria-expanded="false" aria-controls="collapse1">
+    Event Information Echos
+  </a>
+  <a class="btn btn-primary" data-bs-toggle="collapse" href="#collapse2" role="button" aria-expanded="false" aria-controls="collapse2">
+    Player 1 Echos
+  </a>
+  <a class="btn btn-primary" data-bs-toggle="collapse" href="#collapse3" role="button" aria-expanded="false" aria-controls="collapse3">
+    Player 2 Echos
+  </a>
+</p>
+<div class="collapse" id="collapse1">
+  <div class="card card-body">
+  <?php
+//Game Data Display
+
+echo "<strong><u>Event Information</strong></u><br>";
+echo "Game ID: $gameid <br>";
+echo "Event ID: $eventId <br>";
+echo "Event Title: $eventTitle <br>";
+echo "Event Start Date: $eventStart <br>";
+echo "Event End Date: $eventEnd <br>";
+echo "Category: $eventCategory <br>";
+echo "Event Description: $eventDescription<br><br>";
+
+?>
+  </div>
+  </div>
+
+  <div class="collapse" id="collapse2">
+  <div class="card card-body">
+  <?php
+            //Player 1 (TEST) Data Displays
+
+            echo "<u><strong>Player One Data</u></strong><br>";
+
+            echo "Player 1 Name: $player1_name <br>";
+            echo "Player 1 ID: $player1_Id <br>";
+            echo "Player 1 Experience: $player1_experience <br>";
+            echo "Player 1 Gender: $player1_gender <br>";
+            echo "Player 1 DOB: $player1_birth <br>";
+            echo "Player 1 Age: $player1_age <br>";
+            echo "Player 1 Wins: $player1_wins <br>";
+            echo "Player 1 Losses: $player1_losses <br>";
+            echo "Player 1 Org: $player1_org <br>";
+?>
+  </div>
+  </div>
+
+  <div class="collapse" id="collapse3">
+  <div class="card card-body">
+  <?php
+            //Player 2 (TEST) Data Displays
+
+            echo "<strong><u>Player Two Data</u></strong><br>";
+
+            echo "Player 2 Name: $player2_name <br>";
+            echo "Player 2 ID: $player2_Id <br>";
+            echo "Player 2 Experience: $player2_experience <br>";
+            echo "Player 2 Gender: $player2_gender <br>";
+            echo "Player 2 DOB: $player2_birth <br>";
+            echo "Player 2 Age: $player2_age <br>";
+            echo "Player 2 Wins: $player2_wins <br>";
+            echo "Player 2 Losses: $player2_losses <br>";
+            echo "Player 2 Org: $player2_org <br>";
+?>
+  </div>
+  </div>
+
+</div>
+</div>
+    </center><br>
+<!-- -------- END OF TEST ECHOS --------- -->
+
+<hr><br>
+
+<center>
+<strong>Multiple Card View. <br>
+        The card will center if only one is present. Will shrink on smaller screens.<br></strong>
+<br><br></center>
+
+<!-- Start of Card Grid -->
+<div class="row row-cols-1 row-cols-lg-2 g-4 mx-2 px-0 justify-content-center">
+
+<!-- Start of First Card -->
+<div class="col">
+    <div class="shadow p-0 mb-5 bg-white rounded">
+                <div class="card text-center">
+
+                <div id="event-header" class="card-header event"><strong><?php echo $eventTitle; ?></strong></div>
+                    <div class="card-body">
+                    <?php echo "Event ID: $eventId"; ?>
+                    <p class="card-description">
+                    <?php echo "Dates: $eventStart - $eventEnd<br>
+                    $eventCategory<br>
+                    $eventDescription"; ?>
+                    <hr>
+                    </p>
+    <div class="table-responsive">
+                <table class="table table-striped table-sm">
+                <h5 class="card-title">Event Roster</h5><br>
+                <thead class="table-dark">
+                <tr>
+                    <th scope="col" id="event-table-head" class="text-white" data-bs-toggle="modal" data-bs-target="#PlayerInfoModal"><a href="#PlayerInfoModal"><?php echo $player1_name;?></a></th>
+                    <td scope="col" id="event-table-head" class="text-white">vs.</i></th>
+                    <th scope="col" id="event-table-head" class="text-white"><?php echo $player2_name; ?></th>
+                    </tr>
+                </thead>
+                <tbody>
+
+                    <tr>
+                    <td scope="col"><?php echo $player1_gender; ?></td>
+                    <th scope="col">Gender</th>
+                    <td scope="col"><?php echo $player2_gender; ?></td>
+                    </tr>
+
+                    <tr>
+                    <td scope="col"><?php echo $player1_birth; ?></td>
+                    <th scope="col">Birthday</th>
+                    <td scope="col"><?php echo $player2_birth; ?></td>
+                    </tr>
+
+                    <tr>
+                    <td scope="col"><?php echo $player1_age; ?></td>
+                    <th scope="col">Age</th>
+                    <td scope="col"><?php echo $player2_age; ?></td>
+                    </tr>
+
+                    <tr>
+                    <td scope="col"><?php echo $player1_experience; ?></td>
+                    <th scope="col">Experience</th>
+                    <td scope="col"><?php echo $player2_experience; ?></td>
+                    </tr>
+
+                    <tr>
+                    <td scope="col"><?php echo $player1_wins; ?></td>
+                    <th scope="col">Wins</th>
+                    <td scope="col"><?php echo $player2_wins; ?></td>
+                    </tr>
+
+                    <tr>
+                    <td scope="col"><?php echo $player1_losses; ?></td>
+                    <th scope="col">Losses</th>
+                    <td scope="col"><?php echo $player2_losses; ?></td>
+                    </tr>
+
+                    <tr>
+                    <td scope="col"><?php echo $player1_org; ?></td>
+                    <th scope="col">Organization</th>
+                    <td scope="col"><?php echo $player2_org; ?></td>
+                    </tr>
+
+                    <tr>
+                    <td scope="col"><?php echo $player1_Id; ?></td>
+                    <th scope="col"><?php echo "$wpClient #";?></th>
+                    <td scope="col"><?php echo $player2_Id; ?></td>
+                    </tr>
+                </tbody>
+                </table>
+                </div>
+
+                </div>
+                </div>
+            </div>
+        </div>
+
+
+
+<!-- Start of Second Card -->
+
+<div class="col">
+    <div class="shadow p-0 mb-5 bg-white rounded">
+                <div class="card text-center">
+
+                <div id="event-header" class="card-header event"><strong><?php echo $eventTitle; ?></strong></div>
+                    <div class="card-body">
+                    <?php echo "Event ID: $eventId"; ?>
+                    <p class="card-description">
+                    <?php echo "Dates: $eventStart - $eventEnd<br>
+                    $eventCategory<br>
+                    $eventDescription"; ?>
+                    <hr>
+                    </p>
+    <div class="table-responsive">
+                <table class="table table-striped table-sm">
+                <h5 class="card-title">Event Roster</h5><br>
+                <thead class="table-dark">
+                <tr>
+                    <th scope="col" id="event-table-head" class="text-white" data-bs-toggle="modal" data-bs-target="#PlayerInfoModal"><a href="#PlayerInfoModal"><?php echo $player1_name;?></a></th>
+                    <td scope="col" id="event-table-head" class="text-white">vs.</i></th>
+                    <th scope="col" id="event-table-head" class="text-white"><?php echo $player2_name; ?></th>
+                    </tr>
+                </thead>
+                <tbody>
+
+                    <tr>
+                    <td scope="col"><?php echo $player1_gender; ?></td>
+                    <th scope="col">Gender</th>
+                    <td scope="col"><?php echo $player2_gender; ?></td>
+                    </tr>
+
+                    <tr>
+                    <td scope="col"><?php echo $player1_birth; ?></td>
+                    <th scope="col">Birthday</th>
+                    <td scope="col"><?php echo $player2_birth; ?></td>
+                    </tr>
+
+                    <tr>
+                    <td scope="col"><?php echo $player1_age; ?></td>
+                    <th scope="col">Age</th>
+                    <td scope="col"><?php echo $player2_age; ?></td>
+                    </tr>
+
+                    <tr>
+                    <td scope="col"><?php echo $player1_experience; ?></td>
+                    <th scope="col">Experience</th>
+                    <td scope="col"><?php echo $player2_experience; ?></td>
+                    </tr>
+
+                    <tr>
+                    <td scope="col"><?php echo $player1_wins; ?></td>
+                    <th scope="col">Wins</th>
+                    <td scope="col"><?php echo $player2_wins; ?></td>
+                    </tr>
+
+                    <tr>
+                    <td scope="col"><?php echo $player1_losses; ?></td>
+                    <th scope="col">Losses</th>
+                    <td scope="col"><?php echo $player2_losses; ?></td>
+                    </tr>
+
+                    <tr>
+                    <td scope="col"><?php echo $player1_org; ?></td>
+                    <th scope="col">Organization</th>
+                    <td scope="col"><?php echo $player2_org; ?></td>
+                    </tr>
+
+                    <tr>
+                    <td scope="col"><?php echo $player1_Id; ?></td>
+                    <th scope="col"><?php echo "$wpClient #";?></th>
+                    <td scope="col"><?php echo $player2_Id; ?></td>
+                    </tr>
+                </tbody>
+                </table>
+                </div>
+
+                </div>
+                </div>
+            </div>
+        </div>
+
+
+<!-- Start of Third Card -->
+
+<div class="col">
+    <div class="shadow p-0 mb-5 bg-white rounded">
+                <div class="card text-center">
+
+                <div id="event-header" class="card-header event"><strong><?php echo $eventTitle; ?></strong></div>
+                    <div class="card-body">
+                    <?php echo "Event ID: $eventId"; ?>
+                    <p class="card-description">
+                    <?php echo "Dates: $eventStart - $eventEnd<br>
+                    $eventCategory<br>
+                    $eventDescription"; ?>
+                    <hr>
+                    </p>
+    <div class="table-responsive">
+                <table class="table table-striped table-sm">
+                <h5 class="card-title">Event Roster</h5><br>
+                <thead class="table-dark">
+                <tr>
+                    <th scope="col" id="event-table-head" class="text-white" data-bs-toggle="modal" data-bs-target="#PlayerInfoModal"><a href="#PlayerInfoModal"><?php echo $player1_name;?></a></th>
+                    <td scope="col" id="event-table-head" class="text-white">vs.</i></th>
+                    <th scope="col" id="event-table-head" class="text-white"><?php echo $player2_name; ?></th>
+                    </tr>
+                </thead>
+                <tbody>
+
+                    <tr>
+                    <td scope="col"><?php echo $player1_gender; ?></td>
+                    <th scope="col">Gender</th>
+                    <td scope="col"><?php echo $player2_gender; ?></td>
+                    </tr>
+
+                    <tr>
+                    <td scope="col"><?php echo $player1_birth; ?></td>
+                    <th scope="col">Birthday</th>
+                    <td scope="col"><?php echo $player2_birth; ?></td>
+                    </tr>
+
+                    <tr>
+                    <td scope="col"><?php echo $player1_age; ?></td>
+                    <th scope="col">Age</th>
+                    <td scope="col"><?php echo $player2_age; ?></td>
+                    </tr>
+
+                    <tr>
+                    <td scope="col"><?php echo $player1_experience; ?></td>
+                    <th scope="col">Experience</th>
+                    <td scope="col"><?php echo $player2_experience; ?></td>
+                    </tr>
+
+                    <tr>
+                    <td scope="col"><?php echo $player1_wins; ?></td>
+                    <th scope="col">Wins</th>
+                    <td scope="col"><?php echo $player2_wins; ?></td>
+                    </tr>
+
+                    <tr>
+                    <td scope="col"><?php echo $player1_losses; ?></td>
+                    <th scope="col">Losses</th>
+                    <td scope="col"><?php echo $player2_losses; ?></td>
+                    </tr>
+
+                    <tr>
+                    <td scope="col"><?php echo $player1_org; ?></td>
+                    <th scope="col">Organization</th>
+                    <td scope="col"><?php echo $player2_org; ?></td>
+                    </tr>
+
+                    <tr>
+                    <td scope="col"><?php echo $player1_Id; ?></td>
+                    <th scope="col"><?php echo "$wpClient #";?></th>
+                    <td scope="col"><?php echo $player2_Id; ?></td>
+                    </tr>
+                </tbody>
+                </table>
+                </div>
+
+                </div>
+                </div>
+            </div>
+        </div>
+
+
+<!-- End Of All Cards -->
+
+<!-- Final Div to Close Out Grid -->
+    </div>
+<!-- END OF CARD GRID -->
+
+<div class="modal fade" id="PlayerInfoModal" tabindex="-1" aria-labelledby="PlayerInfoLabel" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-scrollable">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="PlayerInfoLabel">Additional Player Info</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <p>I need to figure out how to dynamically insert player data into this modal</p>
+
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- END OF Modal -->
+
+
+<br><br>
+    </body>
+    <footer>
+
+    <script src="/wp15_JS/JQuery/jquery-3.6.0.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.min.js" integrity="sha384-IDwe1+LCz02ROU9k972gdyvl+AESN10+x7tBKgc9I5HFtuNz0wWnPclzo6p9vxnk" crossorigin="anonymous"></script>
+
+
+    </footer>
+    </html>
 
 
 </main>
