4113d865ba2c1f854e6fac5b389dc814029f2ccf
joco-88 <jocodigitalweb@gmail.com>
2022-12-09 15:28:23 -0600

    Updated sandbox.php.

--- public/sandbox.php
+++ public/sandbox.php
@@ -204,8 +204,10 @@ echo "Event Description: $eventDescription<br><br>";
       <th scope="col" id="event-table-head-1" class="text-white" data-bs-toggle="modal" data-bs-target="#PlayerInfoModal"><a href="#PlayerInfoModal">
         <img class="bd-placeholder-img rounded mx-auto" width="70" height="70" src="/media/keek.JPG" aria-label="Placeholder: 70x70" preserveAspectRatio="xMidYMid slice" focusable="false">
                             <rect width="100%" height="100%" fill="#868e96"></rect></img></a></th>
-      <td scope="col" id="event-table-head" class="text-white">vs.</i></th>
-      <!--<th scope="col" id="event-table-head" class="text-white"><?php echo $player2_name; ?></th> -->
+      <td scope="col" id="event-table-head-1" class="text-white">vs.</i></th>
+        <th scope="col" id="event-table-head-1" class="text-white" data-bs-toggle="modal" data-bs-target="#PlayerInfoModal"><a href="#PlayerInfoModal">
+          <img class="bd-placeholder-img rounded mx-auto" width="70" height="70" src="/media/keek.JPG" aria-label="Placeholder: 70x70" preserveAspectRatio="xMidYMid slice" focusable="false">
+                              <rect width="100%" height="100%" fill="#868e96"></rect></img></a></th>
       </tr>
 
 
