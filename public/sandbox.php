2d60a9ef327676793426f2c7d6b00aea3b88af3d
joco-88 <jocodigitalweb@gmail.com>
2022-12-09 13:02:59 -0600

    Updated sandbox.php.

--- public/sandbox.php
+++ public/sandbox.php
@@ -189,9 +189,11 @@ echo "Event Description: $eventDescription<br><br>";
 
         <!-- Example Code -->
 
-        <svg class="bd-placeholder-img rounded float-start" width="75" height="75" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 75x75" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#868e96"></rect><text x="50%" y="50%" fill="#dee2e6" dy=".3em">200x200</text></svg>
+        <svg class="bd-placeholder-img rounded float-start" width="70" height="70" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 70x70" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title>
+          <rect width="100%" height="100%" fill="#868e96"></rect></svg>
 
-        <svg class="bd-placeholder-img rounded float-end" width="75" height="75" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 75x275" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#868e96"></rect><text x="50%" y="50%" fill="#dee2e6" dy=".3em">200x200</text></svg>
+        <svg class="bd-placeholder-img rounded float-end" width="70" height="70" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 70x270" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title>
+          <rect width="100%" height="100%" fill="#868e96"></rect></svg>
 
 
       <!-- End Example Code -->
