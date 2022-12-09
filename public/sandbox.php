a5b72efd3fe2c10c9020b14d765127562bca42df
joco-88 <jocodigitalweb@gmail.com>
2022-12-09 15:32:51 -0600

    Updated sandbox.php.

--- public/sandbox.php
+++ public/sandbox.php
@@ -183,7 +183,7 @@ echo "Event Description: $eventDescription<br><br>";
                     </p>
     <div class="table-responsive">
                 <table class="table table-striped table-sm">
-                <h5 class="card-title">Event Roster</h5><br>
+
                 <thead class="table-dark">
                 <tr>
 
@@ -193,7 +193,7 @@ echo "Event Description: $eventDescription<br><br>";
         <img class="bd-placeholder-img rounded mx-auto" width="70" height="70" src="/media/keek.JPG" aria-label="Placeholder: 70x70" preserveAspectRatio="xMidYMid slice" focusable="false">
                             <rect width="100%" height="100%" fill="#868e96"></rect></img></a></th>
 
-         <td scope="col" id="event-table-head-1">vs.</i></th>
+         <td scope="col" id="event-table-head-1"><h5 class="card-title">Event Roster</h5><br></i></th>
 
         <th scope="col" id="event-table-head-1" class="text-white" data-bs-toggle="modal" data-bs-target="#PlayerInfoModal"><a href="#PlayerInfoModal">
           <img class="bd-placeholder-img rounded mx-auto" width="70" height="70" src="/media/keek.JPG" aria-label="Placeholder: 70x70" preserveAspectRatio="xMidYMid slice" focusable="false">
@@ -203,7 +203,7 @@ echo "Event Description: $eventDescription<br><br>";
 
                     <tr>
                     <th scope="col" id="event-table-head" class="text-white" data-bs-toggle="modal" data-bs-target="#PlayerInfoModal"><a href="#PlayerInfoModal"><?php echo $player1_name;?></a></th>
-                    <td scope="col" id="event-table-head" class="text-white"></i></th>
+                    <td scope="col" id="event-table-head" class="text-white">vs.</i></th>
                     <th scope="col" id="event-table-head" class="text-white"><?php echo $player2_name; ?></th>
                     </tr>
                 </thead>
