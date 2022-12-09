b148cacf119ffa1abe49026b371d348844cfddfa
joco-88 <jocodigitalweb@gmail.com>
2022-12-09 10:34:17 -0600

    Updated header.php.

--- templates/header.php
+++ templates/header.php
@@ -18,9 +18,6 @@ $SiteName="Joco88";
     <link rel="stylesheet" href="/css/stylesheet.css?v=11.15.2022.2">
     <link rel="stylesheet" href="/css/font-awesome/font-awesome.min.css">
 
-    <!-- jQuery with Bootstrap Popper -->
-    <script src="https://code.jquery.com/jquery-3.6.1.min.js" integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ=" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
 
 </head>
     <header>
