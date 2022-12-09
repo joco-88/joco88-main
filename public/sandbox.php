aadb63a7727721919765b52b3fb18a6a5d67aaca
joco-88 <jocodigitalweb@gmail.com>
2022-12-09 13:15:30 -0600

    Updated sandbox.php.

--- public/sandbox.php
+++ public/sandbox.php
@@ -198,7 +198,7 @@ echo "Event Description: $eventDescription<br><br>";
 
       <!-- End Example Code -->
 
-                    <th scope="col" id="event-table-head" class="text-white" data-bs-toggle="modal" data-bs-target="#PlayerInfoModal"><img  id="player-thumbnail" class="img-fluid" src="/media/keek.JPG"</img><br><a href="#PlayerInfoModal"><?php echo $player1_name;?></a></th>
+                    <th scope="col" id="event-table-head" class="text-white" data-bs-toggle="modal" data-bs-target="#PlayerInfoModal"><img id="player-thumbnail" class="img-fluid" src="/media/keek.JPG"</img><br><a href="#PlayerInfoModal"><?php echo $player1_name;?></a></th>
                     <td scope="col" id="event-table-head" class="text-white">vs.</i></th>
                     <th scope="col" id="event-table-head" class="text-white"><?php echo $player2_name; ?></th>
                     </tr>
