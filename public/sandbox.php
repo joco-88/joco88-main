407256f6971961ae79683ad1b7359a2565b0f145
joco-88 <jocodigitalweb@gmail.com>
2022-12-14 12:30:28 -0600

    Updated sandbox.php.

--- public/sandbox.php
+++ public/sandbox.php
@@ -496,6 +496,26 @@ echo "Event Description: $eventDescription<br><br>";
 </div>
 <!-- END OF Modal -->
 
+<!-- START OF Modal JS - Needed to dynamially alter modal content -->
+<script>
+const PlayerInfoModal = document.getElementById('PlayerInfoModal')
+PlayerInfoModal.addEventListener('show.bs.modal', event => {
+  // Button that triggered the modal
+  const button = event.relatedTarget
+  // Extract info from data-bs-* attributes
+  const recipient = button.getAttribute('data-bs-whatever')
+  // If necessary, you could initiate an AJAX request here
+  // and then do the updating in a callback.
+  //
+  // Update the modal's content.
+  const modalTitle = exampleModal.querySelector('.modal-title')
+  const modalBodyInput = exampleModal.querySelector('.modal-body input')
+
+  modalTitle.textContent = `New message to ${recipient}`
+  modalBodyInput.value = recipient
+})
+</script>
+<!-- END OF Modal JS - Needed to dynamially alter modal content -->
 
 <br><br>
     </body>
